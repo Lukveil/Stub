6c220d1d14acee77a8cfb7bbe7f7326c36d1b3a1
Romanov Pavel <rpavel6757@gmail.com>
2024-08-23 14:15:48 +0300

    Stub V0.0.4

--- src/main/java/com/example/newMock/controller/KafkaRest.java
+++ src/main/java/com/example/newMock/controller/KafkaRest.java
@@ -1,9 +1,13 @@
 package com.example.newMock.controller;
 
+import com.example.newMock.model.RequestDTO;
+import com.example.newMock.model.ResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class KafkaRest {
@@ -11,10 +15,18 @@ public class KafkaRest {
     @Autowired
     KafkaProducerService messageProducer;
 
-    @PostMapping("/sendMessage")
-    public String sendMessage(@RequestParam("message") String message){
+    @PostMapping(
+            value = "/sendMessage",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes =MediaType.APPLICATION_JSON_VALUE
+    )
+    public String sendMessage(@RequestBody RequestDTO requestDTO){
+        String message = requestDTO.getMessage();
+        ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO.setMessage(message);
+
         messageProducer.sendMessage("group4", message);
-        return "Message has been sent successfully "+message;
+        return "Message has been sent successfully " + requestDTO;
     }
 
 
---
+++ src/main/java/com/example/newMock/model/ResponseDTO.java
@@ -0,0 +1,13 @@
+package com.example.newMock.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ResponseDTO {
+
+    private String message;
+
+
+}
